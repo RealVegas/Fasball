0346fbba86d1089e4aca0e3308182d82c5d86807
Вадим Голов <grafit-design@inbox.ru>
2024-09-13 21:45:32 +0500

    Код готов

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import pygame
 
 
-CAPTION = "Shoot fastball"
+CAPTION = "Турбо-мяч"
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
-pygame.display.set_caption("Shoot fastball")
+pygame.display.set_caption("Турбо-мяч")
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+icon = pygame.image.load("fast-icon.png")
