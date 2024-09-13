55767d60e4ba3ec8bd45d6c9cbeec3fc637875d0
Вадим Голов <grafit-design@inbox.ru>
2024-09-14 00:49:41 +0500

    Пишу динамику

--- main.py
+++ main.py
@@ -1,4 +1,29 @@
 import pygame
+from random import randint
+
+caption = 'Турбо-мяч'
+screen_width = 800
+screen_height = 500
+
+target_width = 100
+target_height = 100
+
+icon_image = pygame.image.load('images/fast_icon.png')
+ground_image = pygame.image.load('images/background.png')
+pri_target = pygame.image.load('images/primary_ball.png')
+sec_target = pygame.image.load('images/secondary_ball.png')
+
+pri_target = pygame.transform.scale(pri_target, (target_width, target_height))
+sec_target = pygame.transform.scale(sec_target, (target_width, target_height))
+
+pygame.init()
+
+pygame.display.set_caption(caption)
+pygame.display.set_icon(icon_image)
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+target_x = randint(0, screen_width - target_width)
+target_y = randint(0, screen_height - target_height)
 
 running = True
 
