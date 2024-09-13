463c592ad89062eb73c3dff8a45f2b889538bf33
Вадим Голов <grafit-design@inbox.ru>
2024-09-13 22:29:22 +0500

    Код полностью готов

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 import pygame
+from random import randint
 
 
-CAPTION = "Турбо-мяч"
+CAPTION = 'Турбо-мяч'
+ICON = pygame.image.load('images/fast-icon.png')
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
-pygame.display.set_caption("Турбо-мяч")
+
+pygame.display.set_caption(CAPTION)
+pygame.display.set_icon(ICON)
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-icon = pygame.image.load("fast-icon.png")
+target_image = pygame.image.load('images/primary_ball.png')
+backround_image = pygame.image.load('images/background.png')
+target_width = 128
+target_height = 128
+
+target_x = randint(0, SCREEN_WIDTH - target_width)
+target_y = randint(0, SCREEN_HEIGHT - target_height)
+
+screen.blit(backround_image, (0, 0))
+
+pygame.display.flip()
