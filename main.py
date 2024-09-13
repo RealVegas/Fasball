e9fc6af0f43139772b1b401769235f58411f3acf
Вадим Голов <grafit-design@inbox.ru>
2024-09-13 18:04:24 +0500

    add base code

---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame
+
+pygame.init()
+
+pygame.display.set_caption("Shoot fasball")
+screen = pygame.display.set_mode((800, 600))
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+pygame.quit()
