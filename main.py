e27a6728d1bef89746f7018d12ff7453e167ccbd
Вадим Голов <grafit-design@inbox.ru>
2024-09-14 00:42:10 +0500

    Пишу динамику

--- main.py
+++ main.py
@@ -4,10 +4,14 @@ running = True
 
 while running:
 
-    screen.blit(ground-image, (0, 0))
+    screen.blit(ground_image, (0, 0))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+
 
 pygame.quit()
