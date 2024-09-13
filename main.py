74d5e3a7819e6a92b23f26f72fcd96db15b75052
Вадим Голов <grafit-design@inbox.ru>
2024-09-14 00:06:02 +0500

    Полностью оформлено главное окно приложения

--- main.py
+++ main.py
@@ -5,10 +5,16 @@ caption = 'Турбо-мяч'
 screen_width = 800
 screen_height = 500
 
+target_width = 100
+target_height = 100
+
 icon_image = pygame.image.load('images/fast_icon.png')
 background_image = pygame.image.load('images/background.png')
 pri_target = pygame.image.load('images/primary_ball.png')
 sec_target = pygame.image.load('images/secondary_ball.png')
+
+pri_target = pygame.transform.scale(pri_target, (target_width, target_height))
+sec_target = pygame.transform.scale(sec_target, (target_width, target_height))
 
 pygame.init()
 
@@ -16,20 +22,26 @@ pygame.display.set_caption(caption)
 pygame.display.set_icon(icon_image)
 screen = pygame.display.set_mode((screen_width, screen_height))
 
-target_width = 128
-target_height = 128
-
 running = True
 while running:
 
+    target_x = randint(0, screen_width - target_width)
+    target_y = randint(0, screen_height - target_height)
+
+
+
     screen.blit(background_image, (0, 0))
+
+    screen.blit(pri_target, (200, 200))
+    screen.blit(sec_target, (100, 100))
+
+
     pygame.display.flip()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-target_x = randint(0, SCREEN_WIDTH - target_width)
-target_y = randint(0, SCREEN_HEIGHT - target_height)
+
 
 pygame.quit()
