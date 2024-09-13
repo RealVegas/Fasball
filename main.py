c0a72d851bd7bcbb5bc5335ca99146adb26dd1d3
Вадим Голов <grafit-design@inbox.ru>
2024-09-13 18:17:19 +0500

    finish constants

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
-import pygame
 
-pygame.init()
 
-pygame.display.set_caption("Shoot fasball")
-screen = pygame.display.set_mode((800, 600))
 
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+CAPTION = "Shoot fastball"
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
 
-pygame.quit()
+pygame.display.set_caption("Shoot fastball")
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
