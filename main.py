9aef9fa863d8d1ddb83aaf138b15220f32cba1d5
Вадим Голов <grafit-design@inbox.ru>
2024-09-13 23:51:19 +0500

    Полностью оформлено главное окно приложения

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
 import pygame
 from random import randint
 
-CAPTION = 'Турбо-мяч'
-ICON = pygame.image.load('images/fast_icon.png')
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 500
+caption = 'Турбо-мяч'
+screen_width = 800
+screen_height = 500
+
+icon_image = pygame.image.load('images/fast_icon.png')
+target_image = pygame.image.load('images/primary_ball.png')
+background_image = pygame.image.load('images/background.png')
 
 pygame.init()
 
-pygame.display.set_caption(CAPTION)
-pygame.display.set_icon(ICON)
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-target_image = pygame.image.load('images/primary_ball.png')
-backround_image = pygame.image.load('images/background.png')
+pygame.display.set_caption(caption)
+pygame.display.set_icon(icon_image)
+screen = pygame.display.set_mode((screen_width, screen_height))
+
 target_width = 128
 target_height = 128
 
 running = True
 while running:
 
-    screen.blit(backround_image, (0, 0))
+    screen.blit(background_image, (0, 0))
     pygame.display.flip()
 
     for event in pygame.event.get():
