0b80bd2b11d13fa15c332add9f29b5a62baf5f7f
Вадим Голов <grafit-design@inbox.ru>
2024-09-14 11:41:50 +0500

    Сделал переключение мячей

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import pygame
+from random import randint
 
 caption = 'Турбо-мяч'
 screen_width = 800
 screen_height = 500
 
-target_width = 120
-target_height = 120
+target_width = 80
+target_height = 80
 
 icon_image = pygame.image.load('images/fast_icon.png')
 ground_image = pygame.image.load('images/background.png')
 pri_target = pygame.image.load('images/primary_ball.png')
 sec_target = pygame.image.load('images/secondary_ball.png')
-pri_active = pygame.image.load('images/pri_active.png.png')
-sec_active = pygame.image.load('images/sec_active.png.png')
+pri_active = pygame.image.load('images/pri_active.png')
+sec_active = pygame.image.load('images/sec_active.png')
 
 pri_target = pygame.transform.scale(pri_target, (target_width, target_height))
 sec_target = pygame.transform.scale(sec_target, (target_width, target_height))
@@ -30,9 +31,9 @@ target_x = (screen_width - target_width) // 2
 target_y = (screen_height - target_height) // 2
 
 # Скорости перемещения по осям и скорость вращения
-target_speed_x = 5
-target_speed_y = 5
-target_speed_r = 5
+target_speed_x = 3
+target_speed_y = 3
+target_speed_r = 6
 target_angle = 0
 
 # Показ фона
@@ -41,6 +42,7 @@ pygame.display.flip()
 
 # Основной цикл
 running = True
+switch_ball = 1
 
 while running:
 
@@ -50,39 +52,39 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    # Изменение положения мяча
     target_x += target_speed_x
     target_y += target_speed_y
-    # Изменение угла вращения
-    target_angle = (target_angle + target_speed_r) % 360
 
     # Переменные для проверки смены направления движения
     prev_speed_x = target_speed_x
     prev_speed_y = target_speed_y
 
+    # Изменение угла вращения
+    target_angle = (target_angle + target_speed_r) % 360
+
+
     # Проверка столкновения с краями окна и изменение направления
     if target_x <= 0 or target_x + target_width >= screen_width:
         target_speed_x = -target_speed_x
+        switch_ball *= -1
+
+
     if target_y <= 0 or target_y + target_height >= screen_height:
         target_speed_y = -target_speed_y
-
-    if target_speed_x != prev_speed_x or target_speed_y != prev_speed_y:
-        switch_ball = True
-    else:
-        switch_ball = False
+        switch_ball *= -1
 
     # Отрисовка мяча
-    new_rectangle = pri_rotated.get_rect(center=(target_x + target_width // 2, target_y + target_height // 2))
-
-    if switch_ball:
+    if switch_ball == 1:
         pri_rotated = pygame.transform.rotate(pri_target, target_angle)
+        new_rectangle = pri_rotated.get_rect(center=(target_x + target_width // 2, target_y + target_height // 2))
         screen.blit(pri_rotated, new_rectangle.topleft)
 
     else:
         sec_rotated = pygame.transform.rotate(sec_target, target_angle)
+        new_rectangle = sec_rotated.get_rect(center=(target_x + target_width // 2, target_y + target_height // 2))
         screen.blit(sec_rotated, new_rectangle.topleft)
 
-    # Обновление части экрана
+    # Обновление части экрана под мячом
     update_rectangle = pygame.Rect(target_x - 10, target_y - 10, target_width + 20, target_height + 20)
     pygame.display.update(update_rectangle)
 
