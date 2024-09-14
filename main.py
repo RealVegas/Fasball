09da387c45368d921dcad3ac14239b9aceaf834f
Вадим Голов <grafit-design@inbox.ru>
2024-09-14 10:09:55 +0500

    Поменял изображения мячей, добавил картинки для активного состояния мячей

--- main.py
+++ main.py
@@ -11,9 +11,13 @@ icon_image = pygame.image.load('images/fast_icon.png')
 ground_image = pygame.image.load('images/background.png')
 pri_target = pygame.image.load('images/primary_ball.png')
 sec_target = pygame.image.load('images/secondary_ball.png')
+pri_active = pygame.image.load('images/pri_active.png.png')
+sec_active = pygame.image.load('images/sec_active.png.png')
 
 pri_target = pygame.transform.scale(pri_target, (target_width, target_height))
 sec_target = pygame.transform.scale(sec_target, (target_width, target_height))
+pri_active = pygame.transform.scale(pri_active, (target_width, target_height))
+sec_active = pygame.transform.scale(sec_active, (target_width, target_height))
 
 pygame.init()
 
@@ -21,19 +25,23 @@ pygame.display.set_caption(caption)
 pygame.display.set_icon(icon_image)
 screen = pygame.display.set_mode((screen_width, screen_height))
 
+# Начальное положение мяча
 target_x = (screen_width - target_width) // 2
 target_y = (screen_height - target_height) // 2
 
+# Скорости перемещения по осям и скорость вращения
 target_speed_x = 5
 target_speed_y = 5
+target_speed_r = 5
+target_angle = 0
 
-angle = 0
-rotation_speed = 5
-
+# Показ фона
 screen.blit(ground_image, (0, 0))
 pygame.display.flip()
 
+# Основной цикл
 running = True
+
 while running:
 
     screen.blit(ground_image, (0, 0))
@@ -42,12 +50,13 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    # Изменение позиции мяча
+    # Изменение положения мяча
     target_x += target_speed_x
     target_y += target_speed_y
 
-    old_speed_x = target_speed_x
-    old_speed_y = target_speed_y
+    # Переменные для проверки смены направления движения
+    prev_speed_x = target_speed_x
+    prev_speed_y = target_speed_y
 
     # Проверка столкновения с краями окна и изменение направления
     if target_x <= 0 or target_x + target_width >= screen_width:
@@ -55,9 +64,10 @@ while running:
     if target_y <= 0 or target_y + target_height >= screen_height:
         target_speed_y = -target_speed_y
 
-
-    if target_speed_x != old_speed_x or target_speed_y != old_speed_y:
-        switch = True
+    if target_speed_x != prev_speed_x or target_speed_y != prev_speed_y:
+        switch_ball = True
+    else:
+        switch_ball = False
 
 
 
